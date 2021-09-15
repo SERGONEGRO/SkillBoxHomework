3ca2c927cd5f49dbf8891c4a8160a388df72dc05
Сергей Воробьев <kazan1003@gmail.com>
2021-09-15 16:59:22 +0300

    lesson 2 homework completed

--- Lesson_06/Homework_Theme_01/Program.cs
+++ Lesson_06/Homework_Theme_01/Program.cs
@@ -36,7 +36,76 @@ namespace Homework_Theme_01
             // 5. В качестве бонусной части, за дополнительную оплату $50, заказчик просит реализовать 
             //    возможность вывода данных в центре консоли.
 
+                                  /*   РЕШЕНИЕ    */
+
+            string firstname;                         //имя
+            string lastname;                          //фамилия
+            int height;                               //рост
+            byte history; byte math; byte rus;        //оценки
+            float sredbal;                            //средний балл
+          
+            string text = "Введите имя: ";                                 //строка, в которую записывается текст для последующей печати
+            int centerX = (Console.WindowWidth / 2) - (text.Length / 2);   //вычисление центра строки на основе длины текста, установка курсора в центр по оси Х
+            int y = Console.CursorTop;                                     // позиция по оси Y = текущей строке
+
+            /// <summary>
+            /// Функция для печати по центру
+            /// </summary>
+            void CenterPrint(string sometext)
+            {
+                y = Console.CursorTop + 1;
+                centerX = (Console.WindowWidth / 2) - (sometext.Length / 2);
+                Console.SetCursorPosition(centerX, y);
+                Console.WriteLine(sometext);
+                
+
+            }
+
+            /// <summary>
+            /// Функция для ввода по центру
+            /// </summary>
+            string CenterRead()
+            {
+                y = Console.CursorTop + 1;
+                centerX = (Console.WindowWidth / 2) -2 ;
+                Console.SetCursorPosition(centerX, y);
+                string somestring = Console.ReadLine();
+                return somestring;
+                
+            }
+
+            /// <summary>
+            /// вычисление среднего балла
+            /// </summary>
+            float Sredbal(byte a, byte b, byte c)
+            {
+                return sredbal = (float)(a + b + c) / 3;
+            }
+
             
+            CenterPrint("Имя:");                               
+            firstname = CenterRead();                        //ввод имени
+            
+            CenterPrint("Фамилия:");                             
+            lastname = CenterRead();                         //ввод фамилии
+
+            CenterPrint("Возраст: ");                          
+            height = Convert.ToInt32(CenterRead());          //Ввод возраста и преобразование строки в Int
+            
+            CenterPrint("Балл за историю:");                 //балл по истории
+            history = Convert.ToByte(CenterRead());
+
+            CenterPrint("Балл за математику:");              //балл по математике
+            math = Convert.ToByte(CenterRead());
+
+            CenterPrint("Балл за русский язык:");             //балл по рус. язу
+            rus = Convert.ToByte(CenterRead());
+
+            //печать всей информации:
+            CenterPrint($"Студент: {firstname} {lastname}, Средний балл: {Sredbal(rus,math,history):0.0}");
+
+            Console.ReadKey();
+
 
         }
     }
