3e80c2c4ddf3b14caad2f43dbe37f2523f073a05
Negro <kazan1003@gmail.com>
2021-09-17 21:06:14 +0300

    Добавил тему 3

---
+++ Theme3_Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Theme3_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
